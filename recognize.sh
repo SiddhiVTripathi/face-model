65c6aa9dfb3c4830131f08cdf5cf664ee7582f7e
Siddhi Vinayak Tripathi <siddhi19vinayak@gmail.com>
2022-01-28 22:07:17 +0530

    added files, also first commit

---
+++ recognize.sh
@@ -0,0 +1,5 @@
+python3 recognize.py --detector face_detection_model \
+	--embedding-model nn4.small2.v1.t7 \
+	--recognizer output/recognizer.pickle \
+	--le output/le.pickle \
+	--image dataset/others/albert-dera-ILip77SbmOE-unsplash.jpg
